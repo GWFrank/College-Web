f52b320ac12fa13023474b26f38d2830e325a1dc
Frank-KHY <64273013+Frank-KHY@users.noreply.github.com>
2020-07-23 21:18:45 +0800

    2020.07.23 beta

--- common_htmls/footer.js
+++ common_htmls/footer.js
@@ -1,4 +1,6 @@
 document.write( '<footer>\n' );
 document.write( '            雖然這裡整理了很多資訊，但還是記得仔細閱讀臺大官方的文件喔!<br/>\n' );
+document.write( '            更新日期：2020/07/23  beta 0.0 <br/>\n' );
+// document.write( '            更新日期：2020/07/23\n' );
 // document.write( '            聯絡：frank910311@gmail.com\n' );
 document.write( '        </footer>' );
